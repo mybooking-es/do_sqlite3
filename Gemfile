ee60cbdc870e930c5cf3fb80a8fa50c5512e1674
Juan Miqueo <jgil@MacBook-Air-de-Juan.local>
2023-11-11 15:55:22 +0100

    first commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source :rubygems
+
+path '../'
+
+gemspec
+
+platforms :jruby do
+  gem 'jdbc-sqlite3', '>=3.5.8'
+  gem 'do_jdbc',      '0.10.17'
+end
+
+group :development do # Development dependencies (as in the gemspec)
+  gem 'rake'
+end
---
+++ ext/do_sqlite3/do_sqlite3.h
@@ -0,0 +1,22 @@
+#ifndef DO_SQLITE3_H
+#define DO_SQLITE3_H
+
+#include <ruby.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#ifndef _WIN32
+#include <sys/time.h>
+#endif
+#include <locale.h>
+#include <sqlite3.h>
+#include "compat.h"
+
+#ifndef HAVE_SQLITE3_PREPARE_V2
+#define sqlite3_prepare_v2 sqlite3_prepare
+#endif
+
+extern VALUE mDO_Sqlite3;
+extern void Init_do_sqlite3_extension();
+
+#endif
